ab077c3b1374920351dc7cdebd4c9ca2227ab635
Isak <it222hp@student.lnu.se>
2025-10-06 12:12:46 +0200

    Update the filestructure and link the module to the app.js

---
+++ myApplication/js/app.js
@@ -0,0 +1,9 @@
+import '../../myModule/src/js/index.js'
+
+const board = document.createElement('my-board')
+document.body.appendChild(board)
+
+board.setCanvasSize(500, 500)
+board.setPenColor('black', 'white', 'violet', 'yellow', 'lime', 'green', 'blue', 'blueviolet', 'brown', 'grey', 'orange', 'red')
+board.setCanvasColor('white', 'black', 'grey', 'green', 'blue', 'red', 'yellow', 'limegreen')
+board.setPenSize(3, 6, 10)
