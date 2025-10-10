80f35083378b2596ad49dd09b65bef6d8223060f
Isak <it222hp@student.lnu.se>
2025-10-10 09:37:39 +0200

    Continue with  the implementation of the word genarator

--- myApplication/src/components/wordGenerator/wordGenerator-template.js
+++ myApplication/src/components/wordGenerator/wordGenerator-template.js
@@ -1,11 +1,11 @@
 export const template = document.createElement('template')
 
 template.innerHTML = `
-<div id='wordGeneratorContainer'> 
-<h2> Word Generator</h2>
-
-
-</div>
+  <div id="wordGeneratorContainer"> 
+    <h2>Word Generator</h2>
+    <button id="generateWordButton">Generate Word</button>
+    <p id="displayWord"></p>
+  </div>
 
 
 
--- myApplication/src/components/wordGenerator/wordGenerator.js
+++ myApplication/src/components/wordGenerator/wordGenerator.js
@@ -4,6 +4,9 @@ customElements.define('word-generator',
 
   class extends HTMLElement {
     #wordGenerator
+    #generateWordButton
+    #displayWord
+    #randomWords = ['car', 'sun', 'football', 'shark', 'bee', 'robot']
 
     constructor () {
       super()
@@ -11,9 +14,46 @@ customElements.define('word-generator',
       this.attachShadow({ mode: 'open' })
       this.shadowRoot.appendChild(template.content.cloneNode(true))
       this.#wordGenerator = this.shadowRoot.querySelector('#wordGeneratorContainer')
+      this.#generateWordButton = this.shadowRoot.querySelector('#generateWordButton')
+      this.#displayWord = this.shadowRoot.querySelector('#displayWord')
     }
 
+    /**
+   * Called automatically when the custom element is inserted into the DOM.
+   * Initializes event listeners and displays an initial random word.
+   *
+   * @override
+   */
     connectedCallback () {
-        console.log('word-gen added')
+      console.log('word-gen added')
+      this.setupWordEvents()
+      // this.displayRandomWord()
     }
+
+    /**
+   * Sets up UI event listeners for the component.
+   *
+   * Attaches a click event handler to the "Generate Word" button,
+   * which retrieves and displays a new random word each time it’s pressed.
+   *
+   * @private
+   * @returns {void}
+   */
+    setupWordEvents () {
+      this.#generateWordButton.addEventListener('click', () => {
+        console.log('generate word')
+        const randomWord = this.getRandomWord()
+        this.#displayWord.textContent = randomWord
+      })
+    }
+
+    getRandomWord () {
+      const randomIndex = Math.floor(Math.random() * this.#randomWords.length)
+      return this.#randomWords[randomIndex]
+    }
+
+/*     displayRandomWord () {
+      const randomWord = this.getRandomWord()
+      this.#displayWord.textContent = randomWord
+    } */
   })
