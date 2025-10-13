5fd1766fafe55a8326636934f182e580e350b5e5
Isak <it222hp@student.lnu.se>
2025-10-13 16:45:55 +0200

    Refactoring of the playerManager

--- myApplication/src/components/playerManager/playerManager.js
+++ myApplication/src/components/playerManager/playerManager.js
@@ -23,40 +23,54 @@ customElements.define('player-manager',
     }
 
     connectedCallback () {
-        this.addPlayer()
+      this.addPlayer()
+      this.#showPlayerList()
     }
 
     addPlayer () {
       this.#playerNameInput.addEventListener('keydown', (e) => {
         if (e.key === 'Enter') {
-          const playerName = this.#playerNameInput.value.trim()
-
-          if (playerName) {
-            console.log('Name submitted:', playerName)
-            this.#playerNameInput.value = ''
-            if (this.#currentPlayers.length >= 4) {
-              console.log('Max 4 players allowed currently')
-              return
-            }
-
-            this.#currentPlayers.push(playerName)
-            console.log(this.#currentPlayers)
-
-            this.#playerList.innerHTML = ''
-            this.#currentPlayers.forEach((playerName, index) => {
-              const playerListItem = document.createElement('li')
-              playerListItem.textContent = `${playerName} SCORE: ??`
-              playerListItem.dataset.index = index
-              this.#playerList.appendChild(playerListItem)
-            }
-            )
-          }
+          this.#handlePlayerNameSubmission()
         }
+      })
+
+      this.#addNewPlayerButton.addEventListener('click', () => this.#handlePlayerNameSubmission())
+    }
+
+    #handlePlayerNameSubmission () {
+      const playerName = this.#playerNameInput.value.trim()
+      if (!this.#validatePlayerName(playerName)) return
+
+      this.#addPlayerToList(playerName)
+      this.#playerNameInput.value = ''
+      this.#showPlayerList()
+    }
+
+    #validatePlayerName (playerName) {
+      if (!playerName) return false
+      if (this.#currentPlayers.length >= 4) {
+        console.log('Max 4 players allowed')
+        return false
+      }
+      return true
+    }
+
+    #addPlayerToList (playerName) {
+      this.#currentPlayers.push({ name: playerName, score: 0})
+      console.log(`currentplayers ${this.#currentPlayers}`)
+    }
+
+    #showPlayerList () {
+      this.#playerList.innerHTML = ''
+      this.#currentPlayers.forEach((player, index) => {
+        const li = document.createElement('li')
+        li.textContent = `${player.name} SCORE: ${player.score}`
+        li.dataset.index = index
+        this.#playerList.appendChild(li)
       })
     }
 
     removePlayer () {
-        this.#playerList
 
     }
 
